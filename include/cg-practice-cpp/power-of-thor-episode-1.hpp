f0450be3db9ddb3b8abdb091ee0638ec7445fdd3
Ben Lechlitner <otherland@gmail.com>
2019-02-17 20:13:31 -0600

    add header for power of thor puzzle

---
+++ include/cg-practice-cpp/power-of-thor-episode-1.hpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 Ben Lechlitner (otherland@gmail.com)
+ * Distributed under the MIT License.
+ * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+ */
+
+#ifndef POWER_OF_THOR_HPP
+#define POWER_OF_THOR_HPP
+
+namespace power_of_thor
+{
+
+int main();
+
+} // namespace power_of_thor
+
+#endif // POWER_OF_THOR_HPP
